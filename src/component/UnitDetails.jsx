baf53d4631a09632fb4e5d8a4575166da250b218
Muhammed basheer <basheerkalliyath2006@gmail.com>
2024-03-11 00:05:48 +0530

    final

--- src/component/UnitDetails.jsx
+++ src/component/UnitDetails.jsx
@@ -1,7 +1,5 @@
 import React,{ useState,useEffect } from "react";
 
-import { redirect } from "react-router-dom";
-
 import { Link } from 'react-router-dom';
 import { db } from "../../lib/firebase";
 import { collection, getDocs } from "firebase/firestore";
